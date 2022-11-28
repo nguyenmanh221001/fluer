5a3faecde225a02abfd07b4bfbe73426b3743a41
nguyenmanh221001 <119007600+nguyenmanh221001@users.noreply.github.com>
2022-11-28 18:39:04 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo aosp_fleur-user
-add_lunch_combo aosp_fleur-userdebug
-add_lunch_combo aosp_fleur-eng
+add_lunch_combo twrp_fleur-user
+add_lunch_combo twrp_fleur-userdebug
+add_lunch_combo twrp_fleur-eng
